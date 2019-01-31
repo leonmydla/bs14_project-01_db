38c5e3c8c654faf61279317009487c777a4156f3
Leon Mydla <leonmydla@gmail.com>
2019-01-31 03:30:08 +0100

    added jpa datatables support

--- src/main/java/la/lymd/bs14_project01/Bs14Project01Application.java
+++ src/main/java/la/lymd/bs14_project01/Bs14Project01Application.java
@@ -4,8 +4,11 @@ import la.lymd.bs14_project01.services.FillDatabase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.data.jpa.datatables.repository.DataTablesRepositoryFactoryBean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryFactoryBeanClass = DataTablesRepositoryFactoryBean.class)
 public class Bs14Project01Application {
 
     public static void main(String[] args) throws InterruptedException {
